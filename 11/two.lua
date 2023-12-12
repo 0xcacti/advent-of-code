cb41ea3281589fe88771f58b170a19d07d8925f4
0xcacti <0xcacti@gmail.com>
2023-12-11 22:20:05 -0500

    save

--- 11/two.lua
+++ 11/two.lua
@@ -120,20 +120,55 @@ function printGalaxies(galaxies)
     end
 end
 
-function manhattanDistance(x1, y1, x2, y2)
-    return math.abs(x2 - x1) + math.abs(y2 - y1)
+function manhattanDistance(x1, y1, x2, y2, unchanged_rows, unchanged_columns, scale)
+    -- calculate the row distance
+    local row_distance = 0
+    if x1 < x2 then
+        for _, rowIndex in ipairs(unchanged_rows) do
+            if x1 < rowIndex and rowIndex < x2 then
+                row_distance = row_distance + scale - 1
+            end
+        end
+    else
+        for _, rowIndex in ipairs(unchanged_rows) do
+            if x2 < rowIndex and rowIndex < x1 then
+                row_distance = row_distance + scale - 1
+            end
+        end
+    end
+
+    -- calculate the column distance
+    local col_distance = 0
+    if y1 < y2 then
+        for _, colIndex in ipairs(unchanged_columns) do
+            if y1 < colIndex and colIndex < y2 then
+                col_distance = col_distance + scale - 1
+            end
+        end
+    else
+        for _, colIndex in ipairs(unchanged_columns) do
+            if y2 < colIndex and colIndex < y1 then
+                col_distance = col_distance + scale - 1
+            end
+        end
+    end
+
+
+    return math.abs(x2 - x1) + row_distance + math.abs(y2 - y1) + col_distance
 end
 
 function solve()
-    local preMap = loadInput()
-    printMap(preMap)
-    local map = updateMap(preMap)
-    printMap(map)
+    local map = loadInput()
+
+    local unchanged_rows = getUnchangedRows(map)
+    local unchanged_cols = getUnchangedCols(map)
     local galaxies = getGalaxies(map)
     local distance = 0
     for i = 1, #galaxies do
         for j = i + 1, # galaxies do
-            distance = distance + manhattanDistance(galaxies[i][1], galaxies[i][2], galaxies[j][1], galaxies[j][2])
+            distance = distance +
+                manhattanDistance(galaxies[i][1], galaxies[i][2], galaxies[j][1], galaxies[j][2], unchanged_rows,
+                    unchanged_cols, 1000000)
         end
     end
     return distance
