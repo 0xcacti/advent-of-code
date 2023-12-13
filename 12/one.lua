728817ea93333bba6297f1e8448b18bcd1aef093
0xcacti <0xcacti@gmail.com>
2023-12-12 23:22:14 -0500

    save

---
+++ 12/one.lua
@@ -0,0 +1,9 @@
+io.input("input.txt")
+-- _._#_#__#_._#_____ 4,2,5
+-- 3C2, 2C1,5C4
+-- how many ways to create
+-- groups of 4, 2, and 5
+-- from 18 total spots
+-- with 12 total open spots
+-- such that groups have
+-- at least one open spot between them
