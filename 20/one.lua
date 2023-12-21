a68263559f51a9aa4691142c0c43af17e419c8d6
0xcacti <0xcacti@gmail.com>
2023-12-21 00:18:57 -0500

    save

--- 20/one.lua
+++ 20/one.lua
@@ -1,4 +1,5 @@
 require("module")
+require("deque")
 
 function mysplit(inputstr, sep)
     if sep == nil then
@@ -28,7 +29,6 @@ function getInput(path)
             broadcastTargets = mysplit(right, ",")
         else
             t = string.sub(left, 1, 1)
-            print(type)
             name = string.sub(left, 2, #left)
             modules[name] = Module.new(name, t, mysplit(right, ","))
         end
@@ -45,9 +45,58 @@ function solve(path)
             end
         end
     end
-    for _, module in pairs(modules) do
-        print(tostring(module))
+    local low, high = 0, 0
+
+    for i = 1, 1000 do
+        low = low + 1
+        queue = Deque.new()
+        for _, target in ipairs(broadcastTargets) do
+            queue:pushBack({ "broadcaster", target, "low" })
+        end
+        while not queue:empty() do
+            origin, target, pulse = table.unpack(queue:popFront())
+            if pulse == "low" then
+                low = low + 1
+            else
+                high = high + 1
+            end
+            if modules[target] == nil then
+                goto continue
+            end
+            module = modules[target]
+
+            if module.type == "%" then
+                if pulse == "low" then
+                    outgoing = ""
+                    if module.memory == "off" then
+                        module.memory = "on"
+                        outgoing = "high"
+                    else
+                        module.memory = "off"
+                        outgoing = "low"
+                    end
+                    for _, output in ipairs(module.outputs) do
+                        queue:pushBack({ module.name, output, outgoing })
+                    end
+                end
+            elseif module.type == "&" then
+                module.memory[origin] = pulse
+                outgoing = "low"
+                for _, memory in pairs(module.memory) do
+                    if memory == "low" then
+                        outgoing = "high"
+                        break
+                    end
+                end
+                for _, output in ipairs(module.outputs) do
+                    queue:pushBack({ module.name, output, outgoing })
+                end
+            end
+
+            ::continue::
+        end
     end
+    return low * high
 end
 
-print(solve("test_input.txt"))
+print(solve("input.txt"))
