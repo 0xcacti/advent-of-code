6a33e021616bac6d2c1b0e9bcbbc8a97e5c4ecbd
0xcacti <0xcacti@gmail.com>
2024-08-19 01:33:41 -0400

    solve pt 1

--- 2021/lisp/3/soln.lisp
+++ 2021/lisp/3/soln.lisp
@@ -2,16 +2,36 @@
 
 (defparameter *inp* (make-array 0 :fill-pointer 0 :adjustable t))
 
-(with-open-file (stream "~/code/challenges/aoc/2021/lisp/3/test-input.txt" :direction :input)
+(with-open-file (stream "~/code/challenges/aoc/2021/lisp/3/input.txt" :direction :input)
   (loop for line = (read-line stream nil)
         while line do 
-        (vector-push-extend line *inp*)))
+        (vector-push-extend 
+         (map 'list #'(lambda (char) (string char)) line)
+         *inp*)))
 
-(defun solv-one () 
+*inp*
+
+(defun solve-one () 
   "solve day 3 part 1"
-  (let ((gamma "") (epsilon ""))
-    (loop for i from 0 below (length *inp*) do 
-          (let ((line (aref *inp* i)))
-            (
-    )))
+  (let 
+      ((gamma "") 
+       (epsilon ""))
+    (loop for i from 0 to (1- (length (aref *inp* 0))) do 
+        (let ((o-count 0) (i-count 0))
+        (loop for j from 0 to (1- (length *inp*)) do 
+            (if (string= (nth i (aref *inp* j)) "0")
+                (incf o-count)
+                (incf i-count)))
+          (if (> o-count i-count)
+              (setf gamma (concatenate 'string gamma "0")
+                    epsilon (concatenate 'string epsilon "1"))
+              (setf gamma (concatenate 'string gamma "1")
+                    epsilon (concatenate 'string epsilon "0")))))
+    (let ((x (parse-integer gamma :radix 2)) (y (parse-integer epsilon :radix 2)))
+      (return-from solve-one (* x y)))))
 
+(solve-one)
+
+(defun solv-two () 
+  "solve day 3 part 2"
+)
