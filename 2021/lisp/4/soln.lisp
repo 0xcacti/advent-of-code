0044f903d690a1366b9317c8471e126411ca4dc1
0xcacti <0xcacti@gmail.com>
2024-08-20 17:40:19 -0400

    start day 5

---
+++ 2021/lisp/4/soln.lisp
@@ -0,0 +1,15 @@
+(ql:quickload "split-sequence")
+(defparameter *inp-nums* (make-array 0 :fill-pointer 0 :adjustable t))
+
+(with-open-file (stream "~/code/challenges/aoc/2021/lisp/4/test-input.txt" :direction :input)
+  (let ((line (read-line stream nil)))
+    (when line
+      (vector-push-extend (split-sequence:split-sequence #\, line) *inp*))))
+
+(defparameter *inp-boards* (make-array 
+
+
+
+
+*inp*
+
