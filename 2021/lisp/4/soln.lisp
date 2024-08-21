4ac320efebd595f65a34fafc8f53e60691bab42b
0xcacti <0xcacti@gmail.com>
2024-08-21 01:03:11 -0400

    small change

--- 2021/lisp/4/soln.lisp
+++ 2021/lisp/4/soln.lisp
@@ -6,9 +6,12 @@
     (when line
       (vector-push-extend (split-sequence:split-sequence #\, line) *inp*))))
 
-(defparameter *inp-boards* (make-array 
+(defparameter *inp-board-strings* (make-array 0 :fill-pointer 0 :adjustable t))
 
-
+(with-open-file (stream "~/code/challenges/aoc/2021/lisp/4/test-input.txt" :direction :input)
+  (let ((line (read-line stream nil)))
+    (when line
+      (vector-push-extend (split-sequence:split-sequence #\, line) *inp*))))
 
 
 *inp*
