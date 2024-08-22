9f65695593414f09ecf0360d29dfca3221d2cd13
0xcacti <0xcacti@gmail.com>
2024-08-22 14:31:51 -0400

    flatten these suckers

--- 2021/lisp/4/soln.lisp
+++ 2021/lisp/4/soln.lisp
@@ -1,78 +1,39 @@
 (ql:quickload "split-sequence")
+(ql:quickload "cl-ppcre")
+
 (defparameter *inp-nums* (make-array 0 :fill-pointer 0 :adjustable t))
-
-(with-open-file (stream "~/code/challenges/aoc/2021/lisp/4/test-input.txt" :direction :input)
-  (let ((line (read-line stream nil)))
-    (when line
-      (vector-push-extend (split-sequence:split-sequence #\, line) *inp*))))
-
-*inp-nums*
 (defparameter *inp-boards* (make-array 0 :fill-pointer 0 :adjustable t))
 
-
-
-(with-open-file (stream "~/code/challenges/aoc/2021/lisp/4/test-input.txt" :direction :input)
-  (let ((data-str (with-output-to-string (out)
-                    (loop for line = (read-line stream nil)
-                          while line do
-                          (princ line out)
-                          (terpri out)))))
-    (let ((parts (split-sequence:split-sequence #(#\Newline #\Newline) data-str)) (i 0))
-      (loop for part in parts do
-        (if (zerop i)
-            (vector-push-extend (split-sequence:split-sequence #\, part) *inp-boards*)
-            (let ((board (make-array 0 :fill-pointer 0 :adjustable t))) 
-               (loop for line in (split-sequence:split-sequence #\Newline part) do 
-                  (vector-push-extend (split-sequence:split-sequence #\, line) board))
-               (vector-push-extend board *inp-boards*)))
-        (incf i)))))
-
-
-*inp*
+*inp-nums*
 *inp-boards*
 
-
-(format t "womeow~%")
-
 (with-open-file (stream "~/code/challenges/aoc/2021/lisp/4/test-input.txt" :direction :input)
   (let ((data-str (with-output-to-string (out)
                     (loop for line = (read-line stream nil)
                           while line do
                           (princ line out)
                           (terpri out)))))
-    
-    ;; Trim any extraneous whitespace characters from the data string
-    (setf data-str (string-trim '(#\Space #\Tab #\Newline) data-str))
-    (format t "Data read: ~a~%" data-str)  ;; Debug: Print the entire data string
-
-    ;; Attempt to split on both double newlines and spaces
-    (let ((parts (split-sequence:split-sequence '(#\Newline #\Newline) data-str :remove-empty-subseqs t))
+    ;; Use regular expression to split on double newlines
+    (let ((parts (cl-ppcre:split "\\n\\n+" data-str))
           (i 0))
-      (format t "Number of parts: ~d~%" (length parts))  ;; Debug: Print the number of parts
-
       (loop for part in parts do
-            (format t "Processing part ~d: ~a~%" i part)  ;; Debug: Print each part being processed
-
             (if (zerop i)
                 ;; Handle the first part (numbers separated by commas)
                 (progn
-                  (format t "First part (numbers): ~a~%" part)  ;; Debug: Print first part
-                  (vector-push-extend (split-sequence:split-sequence #\, part) *inp-nums*))
+                  (let ((nums (mapcar #'parse-integer (split-sequence:split-sequence #\, part))))
+                  (setf *inp-nums* (apply #'vector nums))))
                 
                 ;; Handle the subsequent parts (5x5 boards)
                 (let ((board (make-array '(5 5))))
-                  (format t "meow~%")  ;; Debug: Indicate that we're entering board creation
-                  (loop for line in (split-sequence:split-sequence #\Newline part) do
+                  (loop for line in (split-sequence:split-sequence #\Newline part)
+                        for row from 0 do
                         (setf line (string-trim '(#\Space #\Tab) line)) ;; Trim leading/trailing spaces
                         (let ((numbers (mapcar #'parse-integer
                                                (remove "" 
-                                                       (split-sequence:split-sequence '(#\Space) line)))))
-                          (format t "Row numbers: ~a~%" numbers)  ;; Debug: Print the numbers in each row
-                          
+                                                       (cl-ppcre:split "\\s+" line)))))  ;; Split on one or more spaces or tabs
                           (loop for num in numbers
                                 for col from 0 do
-                                (setf (aref board i col) num))))
-                  (format t "Board created: ~a~%" board)  ;; Debug: Print the 5x5 board
+                                (setf (aref board row col) num))))
                   (vector-push-extend board *inp-boards*)))
             (incf i)))))
 
