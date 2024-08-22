d1c1899baad9a10658f1ad8011692ade3601c53f
0xcacti <0xcacti@gmail.com>
2024-08-22 13:48:57 -0400

    ugh this is so hard to process this data

--- 2021/lisp/4/soln.lisp
+++ 2021/lisp/4/soln.lisp
@@ -6,12 +6,28 @@
     (when line
       (vector-push-extend (split-sequence:split-sequence #\, line) *inp*))))
 
-(defparameter *inp-board-strings* (make-array 0 :fill-pointer 0 :adjustable t))
+*inp-nums*
+(defparameter *inp-boards* (make-array 0 :fill-pointer 0 :adjustable t))
 
 (with-open-file (stream "~/code/challenges/aoc/2021/lisp/4/test-input.txt" :direction :input)
-  (let ((line (read-line stream nil)))
-    (when line
-      (vector-push-extend (split-sequence:split-sequence #\, line) *inp*))))
+  (let ((data-str ""))
+  (loop for line = (read-line stream nil)
+        while line do 
+        (setf data-str (concatenate 'string data-str line)))
+    (let ((parts (split-sequence:split-sequence #(#\Newline #\Newline) data-str)))
+      (loop for part in parts do
+        (format t "part: ~a~%" part)))))
+
+(with-open-file (stream "~/code/challenges/aoc/2021/lisp/4/test-input.txt" :direction :input)
+  (let ((data-str (with-output-to-string (out)
+                    (loop for line = (read-line stream nil)
+                          while line do
+                          (princ line out)
+                          (terpri out)))))
+    (let ((parts (split-sequence:split-sequence #\Newline data-str :remove-empty-subseqs t)))
+      (loop for part in (split-sequence:split-sequence #\Newline (first parts) :remove-empty-subseqs t) do
+        (format t "part: ~a~%" part)))))
+
 
 
 *inp*
