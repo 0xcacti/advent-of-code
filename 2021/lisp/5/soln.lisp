e7da923a4f65c6e170710e8207422795819c48bc
0xcacti <0xcacti@gmail.com>
2024-08-24 18:18:17 -0400

    finish part 2

--- 2021/lisp/5/soln.lisp
+++ 2021/lisp/5/soln.lisp
@@ -67,7 +67,41 @@
 (solve-one)
 
 
+(defun populate-board-with-diag ()
+  (loop for pair in *pairs* do 
+        (let* ((pair1 (first pair))
+               (pair2 (second pair))
+               (x1 (first pair1))
+               (y1 (second pair1))
+               (x2 (first pair2))
+               (y2 (second pair2)))
+          (cond
+            ;; Handle horizontal line
+            ((= y1 y2)
+             (loop for x from (min x1 x2) to (max x1 x2) do
+                   (incf (aref *board* y1 x) 1)))
+            
+            ;; Handle vertical line
+            ((= x1 x2)
+             (loop for y from (min y1 y2) to (max y1 y2) do
+                   (incf (aref *board* y x1) 1)))
+            
+            ;; Handle diagonal lines (45-degree only)
+            ((= (abs (- x1 x2)) (abs (- y1 y2))) ;; checks if the line is diagonal at 45
+              (let ((dx (if (> x2 x1) 1 -1))  ;; set the x travel direction
+                    (dy (if (> y2 y1) 1 -1))) ;; set the y travel direction
+                (loop for x = x1 then (+ x dx)  ;; loop through the x values
+                      for y = y1 then (+ y dy)    ;; loop through the y values
+                      repeat (1+ (abs (- x1 x2))) do  ;; loop through the absolute difference between the x values
+                      (incf (aref *board* y x) 1)))))))) ;; set the value of the board to 1 if it is on the line
+
+
+
+
 (defun solve-two () 
-  "sole part two day 5")
+  "sole part two day 5"
+  (populate-board-with-diag)
+  (count-twos))
 
 
+(solve-two)
