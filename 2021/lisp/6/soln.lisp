7b90270aca290ef85c47b313273dbba1578f7757
0xcacti <0xcacti@gmail.com>
2024-08-25 14:53:44 -0400

    wow exponential blowup

--- 2021/lisp/6/soln.lisp
+++ 2021/lisp/6/soln.lisp
@@ -1,7 +1,5 @@
 (ql:quickload "cl-ppcre")
 
-(defparameter *input* nil)
-
 (defun read-input (is-test input)
   (let* ((input-file (if is-test "test-input.txt" "input.txt"))
          (path (merge-pathnames input-file #P"~/code/challenges/aoc/2021/lisp/6/")))
@@ -11,25 +9,29 @@
             (let ((parts (mapcar #'parse-integer (cl-ppcre:split "," line))))
               (setf input (nconc input parts)))))) input)
 
-
-
-(read-input t)
-*input*
-
 (defun map-day (input)
-  (mapcar (lambda (x) (if (= x 0) (list 6 8) (- x 1))) input))
+  (mapcar (lambda (x) 
+            (cond 
+             ((listp x) (map-day x))
+             ((= x 0) (list 6 8))
+             (t (- x 1)))) input))
 
 
+(defun count-elements (lst)
+  "recursively count the total number of elements in a list"
+  (cond 
+    ((null lst) 0)
+    ((atom (car lst)) (+ 1 (count-elements (cdr lst))))
+    (t (+ (count-elements (car lst)) (count-elements (cdr lst))))))
 
 
 (defun solve-one ()
   "Solve part one day six"
-  (let ((input nil) (is-test t))
+  (let ((input nil) (is-test nil))
     (setf input (read-input is-test input))
-    (format t "Input: ~a~%" input)
-    (loop for i from 0 to 79 do 
+    (loop for i from 0 to 255 do 
+          (format t "i: ~a~%" i)
           (setf input (map-day input)))
-    (+ (mapcar #'length input))))
-
+    (format t "sum: ~a~%" (count-elements input))))
 
 (solve-one)
