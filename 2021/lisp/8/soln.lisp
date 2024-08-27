76611fcdb10b63b7e2f21c48038a99b42d1a35e6
0xcacti <0xcacti@gmail.com>
2024-08-27 17:11:26 -0400

    work on solving

--- 2021/lisp/8/soln.lisp
+++ 2021/lisp/8/soln.lisp
@@ -32,16 +32,55 @@
             (incf c)))))
     (format t "c: ~a~%" c)))
 
-;; caefbdg :: 8
-;; bfdg :: 
-;; ac :: 1
+;; 0: 6
+;; 6: 6
+;; 9: 6
 
-;; 
-;; len(3) = 7
-;; len(2) = 1
+;; 2: 5
+;; 3: 5
+;; 5: 5
 
-;; aedcg db ecbdgf badfegc abfcde edb cbgfe bfdg bdgec agfbce | ecagd gcbde dbcefa bgfd
-;; bgeadf egcbda cfebg ac caefbdg bacd ecfgad cabeg gca abgde | gcbef befcg ac ac
+;; 1: 2 
+;; 4: 4
+;; 7: 3
+;; 8: 7
+
+;; a: (len(3) - len(2)) 
+;; b: (len(4) - len(2)) 
+;; c: in(len2)) only in 2 of len(6) 
+;; d: not C, in(len(4)), only in 2 of len(6)
+;; e: missing from len(6) and not(C) and not(d) 
+;; f: only missing from 1 digit
+;; g: what's left
+
+
+(defun normalize (digits) 
+  "sorts characters in a string" 
+    (coerce (sort (coerce s 'list) #'char<) 'string))
+
+(defun deduce (digits)
+  "takes a list of strings"
+  (let ((len2 "") (len3 "") (len5 "") (len7 "") (len5 nil) (len6 nil) (digit-values (make-array 10 :initial-element "")))
+    (loop for d in digits do 
+      (cond 
+        ((= (length d) 2) (setf len2 (normalize d)))
+        ((= (length d) 3) (setf len3 (normalize d)))
+        ((= (length d) 5) (setf len5 (normalize d)))
+        ((= (length d) 7) (setf len7 (normalize d)))
+        ((= (length d) 5) (push (normalize d) len5))
+        (t (push (normalize d) len6))))
+
+    (setf (elt digit-values 0) "")
+    (setf (elt digit-values 1) len2)
+    (setf (elt digit-values 3) "")
+    (setf (elt digit-values 4) "")
+    (setf (elt digit-values 5) len4)
+    (setf (elt digit-values 6) "")
+    (setf (elt digit-values 7) len5)
+    (setf (elt digit-values 8) len7)
+    (setf (elt digit-values 9) "")
+
+    ))
 
 
 (solve-one)
