f355116a0b04a51f1a98a6289109adc797b79e69
0xcacti <0xcacti@gmail.com>
2024-09-22 01:50:34 -0400

    save

--- 2021/lisp/11/soln.lisp
+++ 2021/lisp/11/soln.lisp
@@ -1,17 +1,4 @@
-
 (ql:quickload "cl-ppcre")
-
-(defun make-hset (&key (test #'eql))
-  (make-hash-table :test test))
-
-(defun hset-add (item set)
-  (setf (gethash item set) t))
-
-(defun hset-remove (item set)
-  (remhash item set))
-
-(defun hset-contains-p (item set)
-  (gethash item set))
 
 (defun read-input (is-test)
   (let* ((input-file (if is-test "test-input.txt" "input.txt"))
@@ -21,139 +8,108 @@
     (with-open-file (stream path :direction :input)
       (loop for line = (read-line stream nil)
             while line do 
-            (let ((line-array (map 'vector #'digit-char-p line)))
+            (let ((line-array (map 'vector 
+                                    (lambda (c) (- (char-code c) (char-code #\0))) 
+                                    line)))
               (vector-push-extend line-array inp))))
-  (loop for r below (length inp) do 
-        (loop for c below (length (aref inp 0)) do
-          (setf (gethash (list r c) octopuses) (aref (aref inp r) c))))
+    (loop for r below (length inp) do 
+          (loop for c below (length (aref inp 0)) do
+                (setf (gethash (list r c) octopuses) (aref (aref inp r) c))))
+    octopuses))
 
-  octopuses))
 
 (read-input t)
 
 (defun print-octopuses (octopuses) 
-  (loop for r below 5 do
-        (loop for c below 5 do
+  (loop for r below 10 do
+        (loop for c below 10 do
               (format t "~a " (gethash (list r c) octopuses)))
         (format t "~%")))
 
-(print-octopuses (read-input t))
-
-(defun flash-dance (octopuses &aux 
-                              (octopuses) (copy-hash-table octopuses)
-                              (part1 0))
-  (loop for step from 1 for flashes = (dance octopuses)
-        sum flashes into flash-count
-        when (= step 100) do (setf part1 flash-count)
-        when (= flashes 100) return (values part1 step)))
-
-(flash-dance (read-input t))
-
-(defun dance (curr &aux remaining (flashed (make-hset nil :test 'equal)))
-  (flet ((flashesp (energy) (> energy 9))
-         (flash (p)
-           (hset-add p flashed)
-           (setf remaining (append (neighbors curr p) remaining))))
-    (loop for p being the hash-keys of curr
-          when (flashesp (incf (gethash p curr))) do (flash p))
-    (loop while remaining
-          for n = (pop remaining)
-          unless (hset-contains-p n flashed)
-          when (flashesp (incf (gethash n curr))) do (flash n))
-    (loop for p being the hash-keys of curr using (hash-value e)
-          count (when (flashesp e) (setf (gethash p curr) 0)))))
 
 
-(defun adjacents (point &key (include-diagonal nil))
-  "Return the list of adjacent points to the given point (row, col).
-   If :include-diagonal is true, include diagonal neighbors."
-  (let* ((row (first point))
-         (col (second point))
-         (deltas (if include-diagonal
-                     '((-1 -1) (-1 0) (-1 1) (0 -1) (0 1) (1 -1) (1 0) (1 1))
-                     '((0 -1) (0 1) (-1 0) (1 0)))))
-    (loop for (dr dc) in deltas
-          collect (list (+ row dr) (+ col dc)))))
-
-(defun neighbors (octopuses p)
-  (loop for n in (adjacents p :include-diagonal t)
-        when (gethash n octopuses) collect n))
+(defun adjacent-points (point) 
+  (let ((r (first point))
+        (c (second point)))
+    (list (list (- r 1) c)     ;; Up
+          (list (+ r 1) c)     ;; Down
+          (list r (- c 1))     ;; Left
+          (list r (+ c 1))     ;; Right
+          (list (- r 1) (- c 1)) ;; Top-left
+          (list (- r 1) (+ c 1)) ;; Top-right
+          (list (+ r 1) (- c 1)) ;; Bottom-left
+          (list (+ r 1) (+ c 1)) ;; Bottom-right
+          )))
 
 
 
+(defun solve-one () 
+  (let ((octopuses (read-input nil))
+        (num-flashes 0))
+    (loop for i below 100 do 
+        (let ((todo '()))
+      (loop for r below 10 do 
+            (loop for c below 10 do 
+                  (let ((current (gethash (list r c) octopuses)))
+                    (setf (gethash (list r c) octopuses) (+ 1 current)))
+                  (when ( > (gethash (list r c) octopuses) 9)
+                    (push (list r c) todo))
+                  ))
+      (loop while todo do 
+            (let ((point (pop todo)))
+              (when (not (zerop (gethash point octopuses)))
+                (setf (gethash point octopuses) 0)
+                (incf num-flashes 1)
+            (loop for adj in (adjacent-points point) 
+                when (and (gethash adj octopuses)
+                          (not (zerop (gethash adj octopuses))))
+                    do 
+                    (setf (gethash adj octopuses) (+ 1 (gethash adj octopuses)))
+                    (when ( > (gethash adj octopuses) 9)
+                      (push adj todo))))))))
+
+    num-flashes))
 
 
-;; (defun increment-all (grid)
-;;   "increment all elements of a grid"
-;;   (loop for i from 0 below (length grid)
-;;         do (loop for j from 0 below (length (aref grid i))
-;;                  do (incf (aref (aref grid i) j))))
-;;   grid)
-;; 
-;; (increment-all (read-input t))
-;; 
-;; (defun run-flashes (grid flash-count has-flashed row col)
-;;   "Run the flashes"
-;;   (when (and (>= row 0) (< row (length grid))
-;;              (>= col 0) (< col (length (aref grid 0)))
-;;              (not (aref has-flashed row col)))
-;;     (when (> (aref (aref grid row) col) 9)
-;;       (setf (aref has-flashed row col) t)
-;;       (incf flash-count)
-;;       (setf (aref (aref grid row) col) 0)
-;;       (format t "Flashing cell at ~a, ~a~%" row col)
-;;       (loop for dx from -1 to 1 do
-;;         (loop for dy from -1 to 1 do
-;;           (let ((new-row (+ row dx))
-;;                 (new-col (+ col dy)))
-;;             (when (and (>= new-row 0) (< new-row (length grid))
-;;                        (>= new-col 0) (< new-col (length (aref grid 0)))
-;;                        (not (aref has-flashed new-row new-col)))
-;;               (incf (aref (aref grid new-row) new-col))
-;;               (multiple-value-setq (grid flash-count)
-;;                 (run-flashes grid flash-count has-flashed new-row new-col)))))))
-;;     (values grid flash-count)))
-;; 
-;; 
-;; (run-flashes (increment-all (read-input t)) 0 (make-array (list 10 10) :initial-element nil) 0 0)
-;; 
-;; (defun run-step (grid)
-;;   "Run a step"
-;;   (let* ((rows (length grid))
-;;          (cols (length (aref grid 0)))
-;;          (has-flashed (make-array (list rows cols) :initial-element nil))
-;;          (flash-count 0))
-;;     (setf grid (increment-all grid))
-;;     (loop for row from 0 below rows do
-;;       (loop for col from 0 below cols do
-;;         (multiple-value-setq (grid flash-count)
-;;           (run-flashes grid flash-count has-flashed row col))))
-;;     (values grid flash-count)))
-;; 
-;; (run-step (read-input t))
-;; 
-;; (defun print-grid (grid)
-;;   (loop for row across grid do
-;;     (loop for cell across row do
-;;       (format t "~2d " cell))
-;;     (format t "~%")))
-;; 
-;; (let ((grid (increment-all (read-input t))))
-;;   (print-grid grid)
-;;   (format t "Grid dimensions: ~dx~d~%" (length grid) (length (aref grid 0))))
-;; 
-;; (defparameter *test-grid* (read-input t))
-;; (defparameter *flashes* 0)
-;; (multiple-value-bind (new-grid flashes)
-;;     (run-step *test-grid*)
-;;   (setf *test-grid* new-grid)
-;;   (incf *flashes* flashes))
-;; 
-;; 
-;; (multiple-value-bind (new-grid flashes)
-;;     (run-step *test-grid*)
-;;   (setf *test-grid* new-grid)
-;;   (incf *flashes* flashes))
-;; *test-grid*
-;; *flashes*
 
+(solve-one)
+
+
+(defun all-flashed (octopuses)
+  (loop for r below 10 do 
+        (loop for c below 10 do 
+              (when (not (zerop (gethash (list r c) octopuses)))
+                (return-from all-flashed nil))))
+  t)
+
+(defun solve-two () 
+  (let ((octopuses (read-input nil))
+        (all-flashed nil)
+        (step-count 0))
+    (loop while (not all-flashed) do
+        (incf step-count)
+        (let ((todo '()))
+      (loop for r below 10 do 
+            (loop for c below 10 do 
+                  (let ((current (gethash (list r c) octopuses)))
+                    (setf (gethash (list r c) octopuses) (+ 1 current)))
+                  (when ( > (gethash (list r c) octopuses) 9)
+                    (push (list r c) todo))
+                  ))
+      (loop while todo do 
+            (let ((point (pop todo)))
+              (when (not (zerop (gethash point octopuses)))
+                (setf (gethash point octopuses) 0)
+            (loop for adj in (adjacent-points point) 
+                when (and (gethash adj octopuses)
+                          (not (zerop (gethash adj octopuses))))
+                    do 
+                    (setf (gethash adj octopuses) (+ 1 (gethash adj octopuses)))
+                    (when ( > (gethash adj octopuses) 9)
+                      (push adj todo)))))))
+        (when (all-flashed octopuses)
+          (setf all-flashed t)))
+
+    step-count))
+
+(solve-two)
