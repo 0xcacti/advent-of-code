d9c730797fdf83df3bedc187fdc665b33f1f7763
0xcacti <0xcacti@gmail.com>
2024-09-23 12:06:48 -0400

    save it up

--- 2021/lisp/12/soln.lisp
+++ 2021/lisp/12/soln.lisp
@@ -1,19 +1,37 @@
 (ql:quickload "cl-ppcre")
 
+;; use dict that maps node to all the nodes it can go to 
+;; key is start and set is all the possible destinations
+
 (defun read-input (is-test)
   (let* ((input-file (if is-test "test-input.txt" "input.txt"))
-         (path (merge-pathnames input-file #P"~/code/challenges/aoc/2021/lisp/11/"))
-         (inp (make-array 0 :adjustable t :fill-pointer 0))
-         (octopuses (make-hash-table :test 'equal)))
+         (path (merge-pathnames input-file #P"~/code/challenges/aoc/2021/lisp/12/"))
+         (caves (make-hash-table :test 'equal)))
     (with-open-file (stream path :direction :input)
       (loop for line = (read-line stream nil)
             while line do 
-            (let ((line-array (map 'vector 
-                                    (lambda (c) (- (char-code c) (char-code #\0))) 
-                                    line)))
-              (vector-push-extend line-array inp))))
-    (loop for r below (length inp) do 
-          (loop for c below (length (aref inp 0)) do
-                (setf (gethash (list r c) octopuses) (aref (aref inp r) c))))
-    octopuses))
+              (let ((line-array (cl-ppcre:split "-" line)))
+              (setf (gethash (first line-array) caves) (second line-array))
+              (setf (gethash (second line-array) caves) (first line-array)))))
+    caves))
 
+(read-input t)
+
+(defun print-map(caves) 
+    (maphash (lambda (k v) (format t "~a->~a~%" k v)) caves))
+
+(print-map (read-input t))
+
+(defun solve-one () 
+  "Solution to day 12, part one" 
+  (let* ((caves (read-input t))
+         (todo '())
+         (all-paths '()))
+    (push "start" todo)
+    (loop while todo 
+          (let ((path (pop todo)))
+          (when (string= (last path) "end")
+            (push path all-paths))
+
+
+  ))
