7f38fc6da87ac84f904acd950344191ed6d3b29f
0xcacti <0xcacti@gmail.com>
2024-09-30 02:39:50 -0400

    save it up

---
+++ 2021/lisp/13/soln.lisp
@@ -0,0 +1,16 @@
+(ql:quickload "cl-ppcre")
+
+(defun read-input (is-test)
+  (let* ((input-file (if is-test "test-input.txt" "input.txt"))
+         (path (merge-pathnames input-file #P"~/code/challenges/aoc/2021/lisp/12/"))
+         (caves (make-hash-table :test 'equal)))
+    (with-open-file (stream path :direction :input)
+      (loop for line = (read-line stream nil)
+            while line do 
+              (let ((line-array (cl-ppcre:split "-" line)))
+                (push (second line-array) (gethash (first line-array) caves '()))
+                (push (first line-array) (gethash (second line-array) caves '())))))
+    caves))
+
+(read-input t)
+
