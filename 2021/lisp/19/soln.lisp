6826875fa2cb25a28e8079c7aa137c1c2c60568e
0xcacti <0xcacti@gmail.com>
2024-10-17 10:03:50 -0400

    sorta start on 1

---
+++ 2021/lisp/19/soln.lisp
@@ -0,0 +1,19 @@
+(ql:quickload "cl-ppcre")
+
+(defun read-input (is-test)
+  (let* ((input-file (if is-test "test-input.txt" "input.txt"))
+         (path (merge-pathnames input-file #P"~/code/challenges/aoc/2021/lisp/15/"))
+         (grid (if is-test 
+                   (make-array '(10 10) :initial-element 0) 
+                   (make-array '(100 100) :initial-element 0))))
+    (with-open-file (stream path :direction :input)
+        (loop for row from 0 
+            for line = (read-line stream nil)
+            while line do 
+            (loop for col from 0 below (length line) 
+                 for char = (char line col) do 
+                 (setf (aref grid row col) (-(char-code char) (char-code #\0))))))
+    grid))
+
+
+
