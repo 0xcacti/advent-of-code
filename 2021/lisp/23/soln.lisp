1006e906a41f2ab7696fb9841cd8a91d05926904
0xcacti <0xcacti@gmail.com>
2024-10-27 01:13:58 -0400

    wow 23 sucks, I really need to learn and memorize dikstras

--- 2021/lisp/23/soln.lisp
+++ 2021/lisp/23/soln.lisp
@@ -1,1 +1,14 @@
-(defun read-input (is-test))
+(defparameter *room-positions* '(2 4 6 8))  
+(defparameter *energy-costs* '(1 10 100 1000))  
+(defparameter *test-start-state* '((1 2 1 3) 
+                         (0 3 2 0)))
+(defparameter *start-state* '((1 3 2 0)
+                              (2 3 1 0)))
+
+(defstruct state 
+  hallway 
+  rooms 
+  energy)
+
+(defun make-initial-state (input)
+
