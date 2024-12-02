c6e68bf48e80a15cb6d8ae8e4143720d4e591c40
0xcacti <0xcacti@gmail.com>
2024-12-02 17:44:59 -0500

    add utils

---
+++ lisp/src/utils/math.lisp
@@ -0,0 +1,17 @@
+(in-package :advent-of-code/utils)
+
+(defun manhattan-distance (x1 y1 x2 y2) 
+  "Calculate the Manhattan distance between two points"
+  (+ (abs (- x1 x2)) (abs (- y1 y2))))
+
+(defun neighbors-4 (x y)
+  "Return the 4 neighbors of a point"
+  (list (cons x (1- y)) (cons (1- x) y) (cons x (1+ y)) (cons (1+ x) y)))
+
+(defun neighbors-8 (x y) 
+  "Return the 8 neighbors of a point"
+  (defun neighbors-8 (x y)
+  (loop :for dx :in '(-1 0 1)
+        :append (loop :for dy :in '(-1 0 1)
+                     :unless (and (zerop dx) (zerop dy))
+                     :collect (cons (+ x dx) (+ y dy)))))
--- lisp/src/utils/package.lisp
+++ lisp/src/utils/package.lisp
@@ -2,5 +2,8 @@
   (:use :cl :cl-ppcre)
   (:nicknames :aoc/utils)
   (:export 
+    #:manhattan-distance
+    #:neighbors-4
+    #:neighbors-8
     #:get-year-day-from-package
     #:with-input))
