74ec8237394f1cf213f3d1bd4e363918e50ae083
0xcacti <0xcacti@gmail.com>
2025-12-16 02:31:32 -0800

    part 1 day 8

--- c/years/2025/08/part1.c
+++ c/years/2025/08/part1.c
@@ -14,6 +14,11 @@ typedef struct {
   int id;
 } Box;
 
+typedef struct {
+  int box1;
+  int box2;
+} Connection;
+
 double distance(Box a, Box b) {
   return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) + pow(b.z - a.z, 2));
 }
@@ -35,12 +40,22 @@ Box *parse_boxes(char **lines, size_t line_count) {
   return boxes;
 }
 
-// TODO: how to handle already connected, is that a step?
-void find_shortest_distance(Box *boxes, size_t box_count, int *i, int *j) {
+void find_shortest_distance(Box *boxes, size_t box_count, int *i, int *j, Connection *attempted,
+                            int attempt_count) {
   double min_distance = INFINITY;
   for (size_t m = 0; m < box_count; m++) {
     for (size_t n = m + 1; n < box_count; n++) {
-      if (boxes[m].connected && boxes[n].connected) continue;
+      // Check if this pair was already attempted
+      bool already_attempted = false;
+      for (int a = 0; a < attempt_count; a++) {
+        if ((attempted[a].box1 == (int)m && attempted[a].box2 == (int)n) ||
+            (attempted[a].box1 == (int)n && attempted[a].box2 == (int)m)) {
+          already_attempted = true;
+          break;
+        }
+      }
+      if (already_attempted) continue;
+
       double dist = distance(boxes[m], boxes[n]);
       if (dist < min_distance) {
         min_distance = dist;
@@ -52,7 +67,7 @@ void find_shortest_distance(Box *boxes, size_t box_count, int *i, int *j) {
 }
 
 int main() {
-  bool is_test = true;
+  bool is_test = false;
   char *path = NULL;
   int num_connections = 0;
 
@@ -72,22 +87,75 @@ int main() {
     printf("Box %zu: (%.2f, %.2f, %.2f)\n", i + 1, boxes[i].x, boxes[i].y, boxes[i].z);
   }
 
+  Connection *attempted = malloc(num_connections * sizeof(Connection));
+  if (!attempted) exit(EXIT_FAILURE);
+
   for (int conn = 0; conn < num_connections; conn++) {
     int i, j;
-    find_shortest_distance(boxes, lc, &i, &j);
-    boxes[i].connected = true;
-    boxes[j].connected = true;
-    if (boxes[i].id != 0) {
-      boxes[j].id = boxes[i].id;
-    } else if (boxes[j].id != 0) {
-      boxes[i].id = boxes[j].id;
+    find_shortest_distance(boxes, lc, &i, &j, attempted, conn);
+
+    // Record this attempt
+    attempted[conn].box1 = i;
+    attempted[conn].box2 = j;
+
+    if (boxes[i].id != 0 && boxes[i].id == boxes[j].id) {
+      printf("Connection %d: Box(%lf, %lf, %lf) <-> Box(%lf, %lf, %lf) [already connected]\n\n",
+             conn + 1, boxes[i].x, boxes[i].y, boxes[i].z, boxes[j].x, boxes[j].y, boxes[j].z);
     } else {
-      boxes[i].id = conn + 1;
-      boxes[j].id = conn + 1;
+      boxes[i].connected = true;
+      boxes[j].connected = true;
+      int id_i = boxes[i].id;
+      int id_j = boxes[j].id;
+      if (id_i != 0 && id_j != 0 && id_i != id_j) {
+        for (size_t k = 0; k < lc; k++) {
+          if (boxes[k].id == id_j) {
+            boxes[k].id = id_i;
+          }
+        }
+      } else if (id_i != 0) {
+        boxes[j].id = id_i;
+      } else if (id_j != 0) {
+        boxes[i].id = id_j;
+      } else {
+        boxes[i].id = conn + 1;
+        boxes[j].id = conn + 1;
+      }
+      printf("Connection %d: Box(%lf, %lf, %lf) <-> Box(%lf, %lf, %lf)\n\n", conn + 1, boxes[i].x,
+             boxes[i].y, boxes[i].z, boxes[j].x, boxes[j].y, boxes[j].z);
     }
-    printf("Connection %d: Box(%lf, %lf, %lf) <-> Box(%lf, %lf, %lf)\n\n", conn + 1, boxes[i].x,
-           boxes[i].y, boxes[i].z, boxes[j].x, boxes[j].y, boxes[j].z);
   }
+
+  // Find max ID used
+  int max_id = 0;
+  for (size_t i = 0; i < lc; i++) {
+    if (boxes[i].id > max_id) {
+      max_id = boxes[i].id;
+    }
+  }
+
+  int *circuit_sizes = calloc(max_id + 1, sizeof(int));
+  if (!circuit_sizes) exit(EXIT_FAILURE);
+  for (size_t i = 0; i < lc; i++) {
+    if (boxes[i].id != 0) {
+      circuit_sizes[boxes[i].id]++;
+    }
+  }
+
+  int max1 = 0, max2 = 0, max3 = 0;
+  for (int i = 1; i <= max_id; i++) {
+    if (circuit_sizes[i] > max1) {
+      max3 = max2;
+      max2 = max1;
+      max1 = circuit_sizes[i];
+    } else if (circuit_sizes[i] > max2) {
+      max3 = max2;
+      max2 = circuit_sizes[i];
+    } else if (circuit_sizes[i] > max3) {
+      max3 = circuit_sizes[i];
+    }
+  }
+  printf("Top 3 largest circuits: %d, %d, %d\n", max1, max2, max3);
+  printf("Product of their sizes: %d\n", max1 * max2 * max3);
 
   free_lines(all_lines, lc);
   return 0;
